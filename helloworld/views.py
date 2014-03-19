ffe6e509c37ccb6caa8160b92167ff72e9e1812a
Xander <xeelaart@gmail.com>
2014-03-19 09:47:26 +0100

    Created another hello world, now in a seperate template.

--- helloworld/views.py
+++ helloworld/views.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'HelloWorld'}
+
+
+@view_config(route_name='helloworld', renderer='templates/helloworld.pt')
+def my_view(request):
+    return {'message': 'Hello cruel world!'}
